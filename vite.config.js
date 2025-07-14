fb22b12da675aa64b57e054588ef6080e366e80c
Ashot Arutyunyan <ashot.arutyunyan@libernetix.com>
2025-07-14 22:03:06 +0300

    🛠 setup GitHub Pages auto deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = 'https://github.com/arutune/energy-mvp'
+const basePath = '/energy-mvp/'
 
 export default defineConfig({
   plugins: [vue()],
-  base: basePath
+  base: 'https://github.com/arutune/energy-mvp'
 })
