e1c1e4946db56700942b8fc85406d20b636036e7
Ashot Arutyunyan <ashot.arutyunyan@libernetix.com>
2025-07-14 22:00:07 +0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const basePath = 'https://github.com/arutune/energy-mvp'
+
 export default defineConfig({
   plugins: [vue()],
-  root: '.',
-  publicDir: 'public'
+  base: basePath
 })
